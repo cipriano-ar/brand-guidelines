dedec380ceded0c8744b8e29415b8bc40476978a
cmazzieri <cmazzieri@gmail.com>
2015-01-30 08:17:23 +1300

    all files added for first time

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(init);
+
+function init() {
+	$('#book').booklet({
+		closed:true,
+		autoCenter:true,
+		tabs:true,
+		manual:false,
+		overlays:false,
+		hovers:false,
+		pageNumbers:false,
+		pagePadding:0,
+		width:930,
+		height:600
+		
+	});
+}
